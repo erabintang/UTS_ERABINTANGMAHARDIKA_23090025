ee853efdff9a6153f054cf8150badb0b5534b0f7
erabintang <150662777+erabintang@users.noreply.github.com>
2024-05-07 16:27:17 +0700

    Add files via upload

---
+++ UTS_ERABINTANGMAHARDIKA_23090025.py
@@ -0,0 +1,19 @@
+#soal nomor1
+penjual = 5000
+pembeli = 10000
+
+penjumlahan = penjual + pembeli
+print(f'Hasil Penjumlahan : {penjumlahan}')
+
+pengurangan = penjual - pembeli
+print(f'Hasil Pengurangan : {pengurangan}')
+
+pembagian = pembeli / penjual
+print(f'Hasi Pebagian antara penjual pembeli adalah: {pembagian}')
+
+#soal nomor 2
+ tahun = str(input('masukan tahun :'))
+if tahun >= 2024 :
+print('tahun 2024 memang tahun kabisat')
+else:
+    print('tida itu bukan tahun kabisat')
